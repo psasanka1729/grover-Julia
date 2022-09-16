4f51c0308f4739da8a442ab1d51e7724b4e420fa
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2022-09-16 15:35:15 -0500

    Commit before run run4_03092022

--- build/run4_03092022/linear.jl
+++ build/run4_03092022/linear.jl
@@ -4,7 +4,7 @@ using Random
 using PyCall
 
 L = 12;
-Random.seed!(7000)
+Random.seed!(10000)
 
 Rx(theta) = exp(-1im*theta*[1 1;1 1]/2);
 #Rx(theta) = [cos(theta/2) -1im*sin(theta/2) ; -1im*sin(theta/2)  cos(theta/2)];#
@@ -719,9 +719,9 @@ Delta_lst = [];
 Energy_lst = [];
 Entropy_lst = [];
 
-Num = 10;
+Num = 15;
 for i=0:Num
-    delta = (x/160.0)+(1/160.0)*(i/Num)+0.3
+    delta = (x/160.0)+(1/160.0)*(i/Num)
     Op = Grover(delta)
     EIGU = py"eigu"(Op)
     X = string(delta)
