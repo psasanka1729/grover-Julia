65ec54808fe45fab02ad481b0d74087e0756cc4d
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2022-09-18 16:18:34 -0500

    Commit before run 2_energy_index

--- build/2_energy_index/grover_energy_index.jl
+++ build/2_energy_index/grover_energy_index.jl
@@ -8,7 +8,7 @@ L = 12;
 SEED = parse(Int64,ARGS[1]);
 Random.seed!(SEED)
 
-delta = 0.02
+delta = 0.5
 
 Rx(theta) = exp(-1im*theta*[1 1;1 1]/2);
 #Rx(theta) = [cos(theta/2) -1im*sin(theta/2) ; -1im*sin(theta/2)  cos(theta/2)];#
