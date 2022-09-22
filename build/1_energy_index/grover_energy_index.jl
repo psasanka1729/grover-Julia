0f717f1f512274d129f00d7d1b6c937e6d906579
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2022-09-22 10:41:12 -0500

    Commit before run 1_energy_index

--- build/1_energy_index/grover_energy_index.jl
+++ build/1_energy_index/grover_energy_index.jl
@@ -912,7 +912,7 @@ def Write_file(Noise, Energy, Entropy):
     f.write(str(Noise) +'\t'+ str(Energy)+ '\t' + str(Entropy) +'\n')
 """
 
-delta = 0.2
+delta = 0.18
 Op = Grover(delta)
 EIGU = py"eigu"(Op)
 X = string(delta)
