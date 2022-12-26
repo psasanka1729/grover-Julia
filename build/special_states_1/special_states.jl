fd47243d5953915bf444b45d68d509ee32e203ce
Sasanka Dowarah <sxd190113@ganymede.utdallas.edu>
2022-12-26 16:08:49 -0600

    Commit before run special_states_1

--- build/special_states_1/special_states.jl
+++ build/special_states_1/special_states.jl
@@ -1,17 +1,17 @@
-L = 8;
+L = 10;
 
 using Random
 using LinearAlgebra
 using SparseArrays
 using DelimitedFiles
-file = raw"8_Grover_gates_data.txt" # Change for every L.
+file = raw"10_Grover_gates_data.txt" # Change for every L.
 M = readdlm(file)
 Gates_data_1 = M[:,1];
 Gates_data_2 = M[:,2];
 Gates_data_3 = M[:,3];
 
 Number_of_Gates = 2*(2*L^2-6*L+5)+2*L+4*L-4;
-SEED = 4000+parse(Int64,ARGS[1]);
+SEED = parse(Int64,ARGS[1]);
 Random.seed!(SEED)
 NOISE = 2*rand(Float64,Number_of_Gates).-1;
 
